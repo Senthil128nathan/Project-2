1882e5ba89f6a07494b5cb2be21021040e3a4fcc
Senthil128nathan <sn90.senthil@gmail.com>
2022-08-28 14:27:47 +0530

    Adding

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class BankInfo extends AxisBank {
+	private void saving() {
+		System.out.println("SB AC");
+	}
+	private void fixed() {
+		System.out.println("Fixed");
+	}
+	
+	public static void main(String[] args) {
+		BankInfo bd = new BankInfo();
+		
+		bd.saving();
+		bd.fixed();
+		bd.deposit();
+	}
+
+}
